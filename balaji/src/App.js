f239af9b9fd0d421a18063024faaf954e57f620a
balajidepani4729 <balajidepani4729@gmail.com>
2025-09-18 15:17:20 +0530

    first commit

---
+++ balaji/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h2>Counter Example using useState</h2>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
