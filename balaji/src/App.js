bb4efb58e32d59aca7f8340a71b9bcf38c2184a0
balajidepani4729 <balajidepani4729@gmail.com>
2025-09-18 15:39:58 +0530

    first commit

--- balaji/src/App.js
+++ balaji/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="container">
-      <h2>Counter Example using useState</h2>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+import React from 'react';
+function App(){
+  const fruits=['Apple','Banana','Mango','Orange','Grapes'];
+return(
+    <div style={{textAlign:'Center',MarginTop:'50px'}}>
+      <h2>fruit List using map()</h2>
+        <ul Style={{listStyleType:'name',pading:0}}>
+          {fruits.map((fruit,index)=>(
+            <li
+            key={index}
+            Style={{
+              fontSize:'20px',
+              margin:'10px 0',
+              color:'#333',
+              fontFamily:'Arial',
+            }}
+            >
+              {fruits}
+            </li>
+          ))}
+        </ul>
     </div>
-  );
-}
-
-export default App;
+  );}export default App;
